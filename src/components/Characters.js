6db2d76c5d9e23addc5ed1379f41705494404229
Jesus Soria <jesus_soria@usmp.pe>
2024-08-07 17:49:45 -0500

    Poniendo estilos a los bloques
    Co-authored-by: AlbertoSoria1998 <albertosoriasoria74@gmail.com>

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -3,6 +3,7 @@
 export default function Characters(props) {
   const { characters } = props;
 
+  console.log(characters);
   return (
     <div className="characters">
         <h1>Personajes</h1>
@@ -12,7 +13,31 @@ export default function Characters(props) {
             {characters.map((character, index) => (
                 // return implicito
                 <div className="character-container" key={index}>
-                    <p>{character.name}</p>
+                    <div>
+                      <img src={character.image} alt={character.name}></img>
+                    </div>
+
+                    <div>
+                      <h3>{character.name}</h3>
+                      <h6>
+                        {character.status === "Alive" ? (
+                          <>
+                            <span className="alive"/>
+                            Alive
+                          </>
+                        ) : (
+                          <>
+                            <span className="dead"/>
+                            Dead
+                          </>
+                        )}
+
+                      </h6>
+                      <p >
+                        <span className="text-grey">Episodios:</span> 
+                        <span>{character.episode.length}</span>
+                      </p>
+                    </div>
                 </div>
             ))}
         </div>
